7d02dfedba0b1acd93b84382f3f862eebf6826f8
manwoodt <121170372+manwoodt@users.noreply.github.com>
2026-03-24 22:26:21 +0300

    refactoring

--- app/src/main/java/com/study/ally/presentation/screens/passport/AllergensBlock.kt
+++ app/src/main/java/com/study/ally/presentation/screens/passport/AllergensBlock.kt
@@ -19,9 +19,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
-import com.study.ally.ui.theme.Primary
 import com.study.ally.ui.theme.PrimaryContainer
 
 @Composable
--- app/src/main/java/com/study/ally/presentation/screens/symptoms/SymptomsScreen.kt
+++ app/src/main/java/com/study/ally/presentation/screens/symptoms/SymptomsScreen.kt
@@ -19,17 +19,12 @@ import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Slider
-import androidx.compose.material3.SliderDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableFloatStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import com.study.ally.presentation.utils.toIntensityText
 import com.study.ally.ui.theme.Primary
